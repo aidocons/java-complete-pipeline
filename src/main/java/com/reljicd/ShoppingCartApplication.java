5c61e4b45a13e8c95164f72b421d9b6392e33584
Mahdi <jaouadi.mahdi1@gmail.com>
2025-01-27 15:45:17 +0100

    env error testing

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -2,11 +2,16 @@ package com.reljicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 
 @SpringBootApplication
 public class ShoppingCartApplication {
 
+    Dotenv dotenv = Dotenv.load();
+    String value = dotenv.get("VARIABLE_NAME");
     public static void main(String[] args) {
-        SpringApplication.run(ShoppingCartApplication.class, args)
+        SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
