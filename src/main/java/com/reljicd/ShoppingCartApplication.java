19b999a48e08862a77ddec2e67949683821e2a53
aidocons <consultant-butlyai@aidodev.com>
2025-02-04 15:46:41 +0100

    secret error

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -2,16 +2,11 @@ package com.reljicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 
 
 @SpringBootApplication
 public class ShoppingCartApplication {
-
-    Dotenv dotenv = Dotenv.load();
-    int value = Integer.parseInt(dotenv.get("V"));
-    int sum = value + 5;
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
