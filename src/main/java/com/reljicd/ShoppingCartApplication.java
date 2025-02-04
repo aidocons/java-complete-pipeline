8f182970814a0b8144c3f4c412f1f8ff20c5b6f3
Mahdi <jaouadi.mahdi1@gmail.com>
2025-02-04 10:11:33 +0100

    test

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -11,7 +11,8 @@ public class ShoppingCartApplication {
 
     Dotenv dotenv = Dotenv.load();
     int value = dotenv.get("VARIABLE_NAME");
-    int sum = value + 5
+    int sum = value + 5;
+    int sum1=1+2;
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
